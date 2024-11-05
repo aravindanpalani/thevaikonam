afd7e1ae69520c52331234718c233957fb23c2c9
Aravindan <80326482+aravindanpalani@users.noreply.github.com>
2024-11-05 09:59:50 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,25 +6,4 @@ document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
-
-    // Animated Counter
-    const counters = document.querySelectorAll('.counter');
-    counters.forEach(counter => {
-        counter.innerText = '0';
-
-        const updateCounter = () => {
-            const target = +counter.getAttribute('data-target');
-            const count = +counter.innerText;
-            const increment = target / 200;
-
-            if(count < target) {
-                counter.innerText = `${Math.ceil(count + increment)}`;
-                setTimeout(updateCounter, 10);
-            } else {
-                counter.innerText = target;
-            }
-        };
-
-        updateCounter();
-    });
 });
