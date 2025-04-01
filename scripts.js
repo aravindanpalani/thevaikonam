bda68cee0e41cce6357e64009e2fce3e23f35a06
Aravindan <80326482+aravindanpalani@users.noreply.github.com>
2025-04-01 05:37:21 +0000

    base code for new website

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Mobile Menu Toggle
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
